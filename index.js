798ef2a01835814a3ecb934902663f52beef6da6
appJobsYpf <juanfranciscopaez785@gmail.com>
2023-01-20 09:31:54 -0300

    PORT

--- index.js
+++ index.js
@@ -45,19 +45,9 @@ app.use('/auth', usarioRoutes)
 app.use('/', propiedadesRoutes)
 app.use('/api', apiRoutes)
 
-app.use((error, req, res,next) =>{
-    res.locals.mensaje = error.message;
-    const status = error.status || 3306;
-    res.locals.status = status;
-    res.status(status);
-    res.render('error');
-})
-
-//dejar que heroku asigne el puerto 
-const host= '0.0.0.0';
-const port = process.env.PORT ||  3306;
 
 //definir un puerto y arrancar el proyecto
-  app.listen(port, host,() => {
+const port = process.env.PORT || 3000;
+  app.listen(port, () => {
     console.log(`EL SERVIDOR ESTA FUNCIONANDO EN EL PUERTO ${port}`)
 });
