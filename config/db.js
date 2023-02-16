5d5cc19ad5daa4f3942ceaf0c39ca9c0e4efa5e3
appJobsYpf <juanfranciscopaez785@gmail.com>
2023-02-16 10:50:18 -0300

    nuevo

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ import Sequelize from 'sequelize'
 import dotenv from 'dotenv'
 dotenv.config({path: '.env'})
 
-const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS ?? '', {
-    host: process.env.BD_HOST,
+const db = new Sequelize('bienesraices_node_mvc', 'root', '2021GTL2030', {
+    host: '127.0.0.1',
     port: 3306,
     dialect: 'mysql',
     define: {
