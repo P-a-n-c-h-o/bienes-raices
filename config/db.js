2ffcbade15ab4295ff97fc6038a8b7c68f6b7fd0
appJobsYpf <juanfranciscopaez785@gmail.com>
2023-02-17 17:36:24 -0300

    ENV

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config({path: '.env'})
 
 const db = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS ?? '', {
-   host: process.env.BD_host,
+   host: process.env.BD_HOST,
    port: 3306,
    dialect: 'mysql',
    define: {
