16f49fced59615b4e734c5aa1b71c79ec2e5e907
appJobsYpf <juanfranciscopaez785@gmail.com>
2023-03-13 15:41:52 -0300

    db socket

--- config/db.js
+++ config/db.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv'
 dotenv.config({path: '.env'})
 
 const db = new Sequelize('realState', 'root', 'Index.jf2030', {
-   charset: 'utf8mb4', host : 'localhost',  socketPath : ' /tmp/mysql.sock',
+   charset: 'utf8mb4', host : 'localhost',  socketPath : '/var/run/mysqld/mysqld.sock',
    port: 3306,
    dialect: 'mysql',
+   logging: function () {},
    define: {
-      timestamps:true
+      timestamps:true,
+      paranoid: true
    },
    pool: {
       max: 5,   
@@ -15,7 +17,9 @@ const db = new Sequelize('realState', 'root', 'Index.jf2030', {
       acquire: 30000,
       idle: 10000
    },
-   operatorAliases: false
+   operatorAliases: false,
+
+
 
 });
 //Checking connection status
